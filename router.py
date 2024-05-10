80a802bfcc3d3ab5088aeb1db9f7d8c1ddaa6b44
felix920506 <felix920506@gmail.com>
2024-05-11 07:05:51 +0800

    Add Router component

---
+++ router.py
@@ -0,0 +1,71 @@
+"""
+MIT License
+
+Copyright (c) 2021 Zauberzeug GmbH
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+from typing import Callable, Dict, Union
+
+from nicegui import background_tasks, helpers, ui
+
+
+class RouterFrame(ui.element, component='router_frame.js'):
+    pass
+
+
+class Router:
+
+    def __init__(self) -> None:
+        self.routes: Dict[str, Callable] = {}
+        self.content: ui.element = None
+
+    def add(self, path: str):
+        def decorator(func: Callable):
+            self.routes[path] = func
+            return func
+        return decorator
+
+    def open(self, target: Union[Callable, str]) -> None:
+        if isinstance(target, str):
+            path = target
+            builder = self.routes[target]
+        else:
+            path = {v: k for k, v in self.routes.items()}[target]
+            builder = target
+
+        # noinspection PyAsyncCall
+        async def build() -> None:
+            with self.content:
+                ui.run_javascript(f'''
+                    if (window.location.pathname !== "{path}") {{
+                        history.pushState({{page: "{path}"}}, "", "{path}");
+                    }}
+                ''')
+                result = builder()
+                if helpers.is_coroutine_function(builder):
+                    await result
+        self.content.clear()
+        background_tasks.create(build())
+
+    def frame(self) -> ui.element:
+        self.content = RouterFrame().on('open', lambda e: self.open(e.args))
+        return self.content
